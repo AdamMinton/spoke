0a3985a48dda6e82fd38556c5bf6b81a8a0ec852
AdamMinton <53316110+AdamMinton@users.noreply.github.com>
2021-05-27 00:01:07 -0400

    new python to find hub commit and add to manifest

---
+++ .github/workflows/find_and_replace_remote_replace.py
@@ -0,0 +1,39 @@
+import re,sys,git
+
+#Arguement passed in
+commit_arg = str(sys.argv[1]) #'Master'
+hub_repo =  str(sys.argv[2]) #'/Users/adamminton/Documents/GitHub/hub/'
+branch = str(sys.argv[3]) #'main'
+
+#Determine latest commit on hub branch
+repo = git.Repo(hub_repo)
+hub_sha = repo.head.object.hexsha
+
+#Determine which Commit SHA to use
+if commit_arg == 'Master':
+    commit = hub_sha
+else:
+    commit = commit_arg
+
+#Location of Manifest File
+filepath = './manifest.lkml'
+
+#Construct text to replace with
+replace = 'ref: "'+ commit +'"'
+
+#Open Manifest File
+with open(filepath) as f:
+    s = f.read()
+
+#Search with regex to find the ref 
+search = re.search(r"ref: \"(\w*)\"",s)
+
+#Return the result of regex
+find = search.group()
+
+#Replace the string
+s = s.replace(find, replace)
+
+#Write back manifest file with new code
+with open(filepath, "w") as f:
+    f.write(s)
---
+++ manifest.lkml
@@ -0,0 +1,6 @@
+project_name: "firehouse_subs_internal"
+
+remote_dependency: firehouse_subs_hub  {
+  url: "git@github.com:FHSReporting/R-A_Hub_Looker.git"
+  ref: "Master"
+}
